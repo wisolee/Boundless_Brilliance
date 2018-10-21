b722b17a9440a5728dcbb03fbabeb194d4ed588f
Cassia Artanegara <cassiarta@gmail.com>
2018-10-21 12:32:19 -0700

    Revert "Register to login switch "

--- src_code/BoundlessBrilliance/Controller/ViewController.swift
+++ src_code/BoundlessBrilliance/Controller/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UITableViewController {
     
     // launch LoginController whenever "Logout" button is pressed
     @objc func handleLogout() {
+        login-screen-1
         let loginScreenController = LoginScreenController()
         present(loginScreenController, animated: true, completion: nil)
         //might need to delete one of these
