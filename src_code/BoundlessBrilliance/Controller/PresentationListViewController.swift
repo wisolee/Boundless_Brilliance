3b8b6cd44a2d071447c866c63c1a54c2b7809951
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2018-10-28 15:52:14 -0700

    added presentation list view class

---
+++ src_code/BoundlessBrilliance/Controller/PresentationListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PresentationListViewController.swift
+//  BoundlessBrilliance
+//
+//  Created by Vinod Krishnamurthy on 10/28/18.
+//  Copyright © 2018 BoundlessBrilliance. All rights reserved.
+//
+
+import UIKit
+
+class PresentationListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
