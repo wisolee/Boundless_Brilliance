0c5cbfa7cb8d47c2ef7b8e21ac0034ccfce3c26a
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2018-11-05 11:13:17 -0800

    cleaned up code a bit and split up request into its own function

--- src_code/BoundlessBrilliance/Controller/PresentationListViewController.swift
+++ src_code/BoundlessBrilliance/Controller/PresentationListViewController.swift
@@ -18,11 +18,30 @@ class PresentationListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // example of slot request
-        let apiEndpoint: String = "https://10to8.com/api/booking/v2/slot/?start_date=2018-11-12&end_date=2018-11-19&location=https://10to8.com/api/booking/v2/location/242664/&staff=https://10to8.com/api/booking/v2/staff/72695/&service=https://10to8.com/api/booking/v2/service/509961/"
+        doAPIRequest()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func doAPIRequest(){
+        let apiBaseEndpoint: String = "https://10to8.com/api/booking/v2/"
+        
+        let slot: String = "slot/?"
+        let organisation: String = "organisation/?"
+        let service: String = "service/?"
+        let staff: String = "staff/?"
+        let location: String = "location/?"
+        
+        let additional_info: String = "start_date=2018-11-12&end_date=2018-11-19&location=https://10to8.com/api/booking/v2/location/242664/&staff=https://10to8.com/api/booking/v2/staff/72695/&service=https://10to8.com/api/booking/v2/service/509961/"
+        
         
         // authorization header - DON'T CHANGE UNLESS AUTHORIZATION FAILS
+        // test token: fdRiruCVyxvCHwud-kNoocYPv4dXiOpx6qhD0qXWeYpOL1itXrFiImOzmRs3
+        // boundless brilliance token: gwu4bSt-fMRJr1io99N8ZckrAkcQvxfApy7VUuafe0W6NnHiGHAySDX1QGFf
         let auth_headers: HTTPHeaders = ["Authorization": "Token gwu4bSt-fMRJr1io99N8ZckrAkcQvxfApy7VUuafe0W6NnHiGHAySDX1QGFf"]
+        
+        // example of slot request
+        let apiEndpoint: String = apiBaseEndpoint + slot + additional_info
         
         let request = Alamofire.request(apiEndpoint, headers: auth_headers)
             .responseJSON { response in
@@ -30,12 +49,7 @@ class PresentationListViewController: UIViewController {
                 debugPrint(response)
         }
         debugPrint(request)
-        
-        
-
-        // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
