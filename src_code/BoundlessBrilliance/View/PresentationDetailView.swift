1377d263ea4b098aa6a4d633c7593be26416d8c8
Cora Monokandilos <cora.monokandilos@gmail.com>
2018-12-02 18:55:26 -0800

    cleaned code in PresentationDetailController, PresentationDetailView, RegisterView

--- src_code/BoundlessBrilliance/View/PresentationDetailView.swift
+++ src_code/BoundlessBrilliance/View/PresentationDetailView.swift
@@ -9,39 +9,36 @@
 import Foundation
 import UIKit
 
-class PresentationDetailView : UIView{
+class PresentationDetailView : UIView {
     
-    // subview - inputsContainerView
-    let scrollContainer: UIScrollView = {
+    // Enables the scrolling on this page
+    let scroll_container: UIScrollView = {
         let view = UIScrollView()
         view.backgroundColor = UIColor.white
-        // must set up this property otherwise, the specified anchors will not work
         view.translatesAutoresizingMaskIntoConstraints = false
-        //view.layer.cornerRadius = 5
         view.layer.masksToBounds = true
         return view
     }()
     
-    // subview - inputsContainerView
-    let inputsView: UIView = {
+    // Makes the inputsView
+    let inputs_view: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.white
-        // must set up this property otherwise, the specified anchors will not work
         view.translatesAutoresizingMaskIntoConstraints = false
-        //view.layer.cornerRadius = 5
         view.layer.masksToBounds = true
         return view
     }()
 }
 
-// subview - nameSeparatorView
-let SeparatorLine: UIView = {
+// Instantiates the separators
+let separator_line: UIView = {
     let view = UIView()
     view.backgroundColor = UIColor(r: 220, g: 220, b: 220)
     view.translatesAutoresizingMaskIntoConstraints = false
     return view
 }()
 
+// Instantiates for Sticker and Shirt options for the spinner
 extension UITextField {
     func loadStickerShirtOptions(spinnerOptions: [String]) {
         self.inputView = SpinnerView(spinnerOptions: spinnerOptions, spinnerTextField: self)
